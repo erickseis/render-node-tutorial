8cf0dc17f60c6feb5ec06bc9d7c5ac305141e9b9
erick seis <erickseis@gmail.com>
2024-05-25 12:04:48 -0400

    second comment

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
+import pg from 'pg'
+import { config } from "dotenv";
+
+config()
 
 const app = express();
 
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL, /*hacemos la conexion de nuestra base de datos que esta en render a nuestra aplicacion o proyecto */
+    ssl: true
+})
 app.get('/', (req, res) => {
-    res.send('hello worl')
+    res.send('hello world')
+})
+
+app.get('/ping', async (req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 })
 
 app.listen(3000)
